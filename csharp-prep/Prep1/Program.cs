238e2c48bf5760a6a686db0e001567d605bb7365
colin24-2003 <clough24@byui.edu>
2025-04-25 15:00:02 -0600

    made changes to prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,29 @@
 using System;
 
+
+/* What is your first name? Scott
+
+What is your last name? Burton
+
+Your name is Burton, Scott Burton.
+
+This is what the output is expected to be*/
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?: ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name?: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
+
+/* first_name = input("What is your first name?: )
+last_name = input("What is your last name?: )
+print(f"Your name is {last_name}, {first_name} {last_name}")
+
+I wrote the program in python first to give me an idea of how it might need to be formatted
+*/
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // A program to computer the area of a circle 
+
+        // Get radius from user
+        Console.Write("Enter the radius of the circle: ");
+        // readLine is  getting input from user
+        string userInput = Console.ReadLine();
+        // Parse is typecasting from string into double 
+        double radius = double.Parse(userInput);
+        //calculating area
+        double area = Math.PI * radius * radius;
+        /* int x = 3;
+         string s = "Goodbye";
+         float f = 3.14F;
+         double d = 5.21;
+         long n = 25; */
+        // Below is c# version of an F-string
+        // "" is string '' is 1 char
+        Console.WriteLine($"Area of the circle: {Math.Round(area, 2)}");
     }
 }
