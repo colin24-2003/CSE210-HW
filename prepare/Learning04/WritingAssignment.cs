b862a65bfa73050cf04dee6621e66b0f66fd2b65
colin24-2003 <clough24@byui.edu>
2025-06-04 15:39:55 -0600

    Writing class

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,18 @@
+public class WritingAssignment : Assignments
+{
+    private string _title;
+
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+
+   public string  GetWritingInformation()
+    {
+        return $"{StudentName} - {Topic}\n {_title} {StudentName}";
+    }
+
+}
