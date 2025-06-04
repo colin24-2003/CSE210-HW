77f2b0f89c06bfee5c689cd5270ee1c1ab88ba6f
colin24-2003 <clough24@byui.edu>
2025-06-04 15:44:59 -0600

    Program class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignments assignments = new Assignments("Colin", "Math"); // inserting values as parameters in the new object 
-        MathAssignment mathAssignment1 = new MathAssignment("Colin", "Fractions", "Section 7.3", "8-19");
-        WritingAssignment writingAssignment1 = new WritingAssignment("Colin", "English", "The Causes of World War II by");
-        Console.WriteLine(assignments.GetSummary()); // object.doSomething with the command
-        Console.WriteLine(mathAssignment1.GetHomeworkList());
-        Console.WriteLine(writingAssignment1.GetWritingInformation());
+        Assignments a1 = new Assignments("Samuel Bennett", "Multiplication"); // inserting values as parameters in the new object 
+        MathAssignment m1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        WritingAssignment w1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a1.GetSummary()); // object.doSomething with the command
+        Console.WriteLine(m1.GetHomeworkList());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
