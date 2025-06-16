29e10ad5ad3aaa677f88eae524ba791524a9cbfe
colin24-2003 <clough24@byui.edu>
2025-06-16 13:03:29 -0600

    Program class to run the polymorphism activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,21 @@ using System;
 
 class Program
 {
+    public static List<Shape> shapes = new List<Shape>();  // Field of the class
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("White", 5.3);
+        Rectangle rectangle = new Rectangle("Black", 10.5, 20.5);
+        Circle circle = new Circle("Blue", 2.5);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.Color}, {shape.GetArea()}");
+        }
     }
 }
